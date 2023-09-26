a17f0fed7c3bff4ccfe6e5705546c75be9b582ae
kirill <kirillaromad@mail.ru>
2023-09-27 01:41:36 +0300

    Rate-limit example

---
+++ PollyExamples.RateLimitApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using Polly;
+using Polly.RateLimit;
+
+// 5 запросов в 1 секунду
+var rateLimitPolicy = Policy
+    .RateLimitAsync(5, TimeSpan.FromSeconds(1));
+
+for (var i = 0; i < 5; i++)
+{
+    try
+    {
+        await rateLimitPolicy.ExecuteAsync(async () =>
+        {
+            await Task.Delay(10); // 10 миллисекунд
+            Console.WriteLine($"Request {i + 1}: OK");
+        });
+    }
+    catch (RateLimitRejectedException ex)
+    {
+        Console.WriteLine($"Request {i + 1}: RateLimitRejectedException - Retry after: {ex.RetryAfter}");
+    }
+}
